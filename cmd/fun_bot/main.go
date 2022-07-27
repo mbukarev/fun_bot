53cc4222206a4f289e2e19b3d9bda10321442385
unknown <test1@DESKTOP-3L79Q7J.(none)>
2022-07-27 10:39:47 +0700

    ttt

--- cmd/fun_bot/main.go
+++ cmd/fun_bot/main.go
@@ -1,13 +1,11 @@
 package main
+
 import (
-  "fmt"
-  //"https://github.com/mbukarev/fun_bot/configs"
-  "fun_bot/pkg/Config"
+	. "fun_bot/pkg/Config"
 )
 
-func main(){
-  var cfg Config
-  cfg.getConfig("../configs/config.yaml")
-  fmt.Print(cfg.tokenTelega)
-
+func main() {
+	var cfg Config
+	cfg.GetConfig("configs/config.yaml")
+	
 }
--- pkg/Config/config.go
+++ pkg/Config/config.go
@@ -1,24 +1,28 @@
-package config
-import (
-  "os"
-  "github.com/go-yaml/yaml"
+package Config
 
+import (
+	"fmt"
+	"github.com/go-yaml/yaml"
+	"os"
 )
-type Config struct{
-  token string `envconfig:"tokenTelega"`
+
+type Config struct {
+	Token string `yaml:"TokenTelega"`
 }
 
-func (c *Config) getConfig (file string) {
-  f, err := os.Open(file)
-  if err != nil {
-      processError(err)
-  }
-  defer f.Close()
+func (c *Config) GetConfig(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		processError(err)
+	}
+	defer f.Close()
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&c)
+	if err != nil {
+		processError(err)
+	}
+}
 
-  var cfg Config
-  decoder := yaml.NewDecoder(f)
-  err = decoder.Decode(&c)
-  if err != nil {
-      processError(err)
-  }
+func processError(err error) {
+	fmt.Println(err)
 }
